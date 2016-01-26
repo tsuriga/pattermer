75d3bc9cfbc582629f0579529593952e3749fd46
Tsuri Kamppuri <royaltroyka@gmail.com>
2016-01-26 03:55:54 +0200

    Initial commit with UI draft

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# pattermer
+# PATTERMER
+
+Pattermer is a mass file renaming desktop program that uses regular expression
+searches mapped to variables by users to rename files according to a given
+pattern.
+
+It is built using HTML5 technologies on Electron and Photon.
+
+* [Electron](http://electron.atom.io/)
+* [Photon](http://photonkit.com/)
+
+## CHANGELOG ##
+
+* alpha-v0.0.1 (2016-01-26): initial commit with UI draft
---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        'min-width': 800,
+        'min-height': 400,
+        'accept-first-mouse': true,
+        'title-bar-style': 'hidden'
+    });
+
+    mainWindow.loadURL('file://' + __dirname + '/app.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
