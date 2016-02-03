023b3034fd65714f9d2761193002646b963eda42
Tsuri Kamppuri <royaltroyka@gmail.com>
2016-02-03 05:26:16 +0200

    Add preliminary UI functionality to add expressions

--- main.js
+++ main.js
@@ -20,20 +20,18 @@ function createWindow() {
 
     mainWindow.openDevTools();
 
-    mainWindow.on('closed', function () {
-        mainWindow = null;
-    });
+    mainWindow.on('closed', () => mainWindow = null);
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on('activate', function () {
+app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
